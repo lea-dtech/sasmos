f36c9a4d8617d1010362b84234f735eeabd07989
Vikram Kumar <84438166+lea-dtech@users.noreply.github.com>
2022-01-03 19:31:06 +0530

    03012022.1.5
    Database port number added

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("ec2-174-129-37-144.compute-1.amazonaws.com", "wflxutwoazkoco", "4a2bc0bd18956f8cd862c063845fee1f117bc02c509f1bcfe6b764edfc9b8f05", "d4l6ft91j0mfc");
+$conn = mysqli_connect("ec2-174-129-37-144.compute-1.amazonaws.com", "wflxutwoazkoco", "4a2bc0bd18956f8cd862c063845fee1f117bc02c509f1bcfe6b764edfc9b8f05", "d4l6ft91j0mfc", 5432);
 
 if (mysqli_connect_error()) {
     echo "Failed to connect to MySQL! Please contact the admin.";
